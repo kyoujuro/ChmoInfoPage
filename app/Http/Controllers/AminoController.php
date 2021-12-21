26824d28c6a197c20d8a9c7aa0389ca210d5e7aa
kyoujuro <shakemaru0138550@gmail.com>
2021-12-21 23:24:19 +0900

    add

--- app/Http/Controllers/AminoController.php
+++ app/Http/Controllers/AminoController.php
@@ -1,13 +1,17 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
 
 class AminoController extends Controller
 {
     //
-    public function index(){
+    public function index(Request $request){
+        $validator = Validator::make($request->all(), [
+            'name' => 'required| stringS',
+            'age' => 'requires | integer | between:0,150',
+        ]);
         if(session('mode') =='amino'){
         return view('amino');
         }
